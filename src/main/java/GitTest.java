4a974473f4b1262a1fb639af8c9653b7ba7d647e
ProcessH <ProcessH@atguigu.com>
2022-07-20 15:06:58 +0800

    fix commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello hot-fix1!");
         System.out.println("hello master1!");
         System.out.println("hello hot-fix2!");
+        System.out.println("就是我改的!");
     }
 }
